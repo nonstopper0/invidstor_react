6130a3c934640c6c10354d69c1860c564ec11e70
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2020-04-19 08:45:25 -0600

    creating basic layout

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import { HashRouter, Link, Route } from 'react-router-dom'
 import Home from './Components/Home.js'
 import './App.css';
 
-function App() {
-  return (
-    <HashRouter>
-        <header className='header'>
-            <Link to="/">Home</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/shop">Shop</Link>
-            <Link to="/portfolio">Portfolio</Link>
-        </header>
-        <Route exact path="/" component={Home} />
-    </HashRouter>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <HashRouter>
+          <header>
+              <Link to="/">Home</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to="/shop">Shop</Link>
+              <Link to="/portfolio">Portfolio</Link>
+          </header>
+          <Route exact path="/" component={Home} />
+      </HashRouter>
+    );
+  }
 }
-
-export default App;
