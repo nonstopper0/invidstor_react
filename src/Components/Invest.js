78d0c6d95882cee3bbfd37f5a510bcc588ee0e76
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2020-05-05 20:21:10 -0600

    added loading screen and finished URLParser

--- src/Components/Invest.js
+++ src/Components/Invest.js
@@ -1,26 +1,52 @@
 import React from 'react'
 import './Invest.css'
 import { IoIosSettings, IoIosHelp, IoMdPlay } from 'react-icons/io'
+import { AiOutlineLoading3Quarters, AiOutlineLoading } from 'react-icons/ai'
 
 export default class Home extends React.Component {
     constructor() {
         super()
         this.state = {
             url: false,
-            loading: true,
+            loading: false,
+            message: false,
         }
     }
     urlParser = (url) => {
         let newURL = ''
+        // if the url is the normal url string...
         if (url.split("")[8] === 'w') {
-            newURL = url.match('/(?<=\=).*')
-            console.log(newURL)
-        } else {
-            console.log('no match', url)
+            const regex = /(?<=\=).*/gm;
+            newURL = url.match(regex)
+        } 
+        // else if the url string is from the share button (condensed)
+        else {
+            const regex = /(?<=be\/).*/gm;
+            newURL = url.match(regex)
         }
-        return newURL
+
+        // check if URL seems valid
+        if (newURL.length > 11) {
+            return false
+        }
+        return newURL.toString()
     }
-    getVideoData = (url) => {
+    getVideoData = async(url) => {
+        this.setState({
+            loading: true
+        })
+        await fetch(`${process.env.REACT_APP_NODE_URL}/youtube/video`, {
+            method: 'POST',
+            body: JSON.stringify({videoID: url}),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(res => res.json())
+          .then(json => console.log(json))
+        this.setState({
+            loading: false
+        })
     }
     handleChange = (e) => {
         this.setState({
@@ -30,25 +56,34 @@ export default class Home extends React.Component {
     handleSubmit = async (e) => {
         e.preventDefault()
         let parsedUrl = await this.urlParser(this.state.url)
-        this.getVideoData(parsedUrl)
+        if (parsedUrl) {
+            this.getVideoData(parsedUrl)
+        }
     }
     render() {
         return (
             <React.Fragment>
                 <div className="investDataContainer">
+                    { !this.state.loading ? 
+                        <div className="investInputContainer">
+                            <h2>INVESTOR</h2>
+                            <p>Paste the video url below to begin</p>
+                            <form onSubmit={this.handleSubmit}>
+                                <input 
+                                    onChange={this.handleChange}
+                                    name="url"
+                                    placeholder="Video Url"
+                                />
+                                <button><IoMdPlay /></button>
+                            </form>
+                            <h6>Video must be no more than 24 hours old and the poster must have atleast 4 videos</h6>
+                            <h6>Example: https://www.youtube.com/watch?v=ycPr5-27vSI</h6>
+                        </div>
+                    :
                     <div className="investInputContainer">
-                        <h2><IoIosSettings className="investSpinner"/>INVESTOR<IoIosSettings className="investSpinner"/></h2>
-                        <p>Paste the video url below to begin</p>
-                        <form onSubmit={this.handleSubmit}>
-                            <input 
-                                onChange={this.handleChange}
-                                name="url"
-                                placeholder="Video Url"
-                            />
-                            <button><IoMdPlay /></button>
-                        </form>
-                        <h6>Example: https://www.youtube.com/watch?v=ycPr5-27vSI</h6>
+                        <AiOutlineLoading3Quarters id="inputSpinner" />
                     </div>
+                    }
                 </div>
             </React.Fragment>
         )
