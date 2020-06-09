a9bff217d5da28b954a88e96432cc8793cb56383
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2020-06-09 01:33:38 -0600

    added user profile updating and display

--- src/Components/MyProfile.js
+++ src/Components/MyProfile.js
@@ -8,7 +8,10 @@ export default class MyProfile extends React.Component {
         super()
         this.state = {
             loading: true,
+            editing: true,
             userData: {},
+            username: '',
+            email: '',
         }
     }
     componentDidMount() {
@@ -25,7 +28,11 @@ export default class MyProfile extends React.Component {
             .then(json => {
                 if (json.status === true) {
                     console.log(json.userInfo)
-                    this.setState({userData: json.userInfo})
+                    this.setState({
+                        userData: json.userInfo,
+                        email: json.userInfo.email,
+                        username: json.userInfo.username
+                    })
                 } else {
                     console.log(json.message)
                 }
@@ -36,8 +43,8 @@ export default class MyProfile extends React.Component {
         })
     }
     handleSubmit = (e) => {
-        console.log('submitted')
         e.preventDefault()
+        this.setState({editing: false})
         this.updateProfile()
     }
     handleChange = (e) => {
@@ -45,24 +52,86 @@ export default class MyProfile extends React.Component {
             [e.target.name]: e.target.value
         })
     }
-    updateProfile = (e) => {
-
+    updateProfile = async(e) => {
+        await fetch(`${process.env.REACT_APP_NODE_URL}/user/update`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                sessionID: this.props.token,
+                updateInfo: {
+                    email: this.state.email,
+                    username: this.state.username
+                }
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(response => response.json())
+          .then(json => {
+              if (json.status === true) {
+                  this.getProfile(this.props.token)
+              }
+          })
     }
     render() {
         return (
             <React.Fragment>
                 <div className="investDataContainer">
                 { !this.state.loading ? 
+
                         <div className="investDataContainerMyProfileContainer">
-                            <div className="myProfileRow">
-                                <h2>Username: <span className="myProfileSpan">{this.state.userData.username}</span></h2>
+
+                            { !this.state.editing ? 
+
+                            <div>
+                                <div className="myProfileRow">
+                                    <h2>Username: <span className="myProfileSpan">{this.state.userData.username}</span></h2>
+                                </div>
+                                <div className="myProfileRow">
+                                    <h2>Joined: <span className="myProfileSpan">{(this.state.userData.created_on).split("").slice(0, 10)}</span></h2>
+                                </div>
+                                <div className="myProfileRow">
+                                    <h2>Email: <span className="myProfileSpan">{this.state.userData.email}</span></h2>
+                                </div>
+                                <div className="myProfileRow">
+                                    <h2>Credits: <span className="myProfileSpan">{this.state.userData.credits}</span></h2>
+                                </div>
                             </div>
-                            <div className="myProfileRow">
-                                <h2>Bio: <span className="myProfileSpan">{this.state.userData.bio}</span></h2>
-                            </div>
-                            <div className="myProfileRow">
-                                <h2>Email: <span className="myProfileSpan">{this.state.userData.email}</span></h2>
-                            </div>
+
+                            :
+
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="myProfileEditingRow">
+                                    <h2>Username</h2>
+                                    <input 
+                                        name="username"
+                                        className="myProfileEditingRow" 
+                                        value={this.state.username}
+                                        placeholder={this.state.userData.username}
+                                        onChange={this.handleChange}
+                                    />
+                                </div>
+                                <div className="myProfileRow">
+                                    <h2>Joined: <span className="myProfileSpan">{(this.state.userData.created_on).split("").slice(0, 10)}</span></h2>
+                                </div>
+                                <div className="myProfileEditingRow">
+                                    <h2>Email</h2>
+                                    <input 
+                                        name="email"
+                                        className="myProfileEditingRow" 
+                                        value={this.state.email}
+                                        placeholder={this.state.userData.email}
+                                        onChange={this.handleChange}
+                                    />
+                                </div>
+                                <div className="myProfileRow">
+                                    <h2>Credits: <span className="myProfileSpan">{this.state.userData.credits}</span></h2>
+                                </div>
+                                <button>Submit</button>
+                            </form>
+
+                            }
+
                         </div>
                 :
                     <div className="investInputContainer">
