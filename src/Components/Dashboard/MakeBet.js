9bf5d753076d85ce507f546f7d9c5f54114f415e
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2020-07-13 11:50:39 -0600

    added submit bet button and logic for bet

--- src/Components/Dashboard/MakeBet.js
+++ src/Components/Dashboard/MakeBet.js
@@ -11,17 +11,25 @@ export default class MakeBet extends React.Component {
             betViews: 0,
             betLikes: 0,
             betDislikes: 0,
+            betAmount: 0,
         }
     }
+
     numberWithCommas = (x) => {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
+
     updateValue = (e) => {
         // middle value is 50, at 50 the number should be equal to the average
         const multiplicationValue = parseFloat(`${e.target.value / 50}`)
         const newViews = (multiplicationValue * this.props.data.betAssesment.averageViews).toFixed(0)
         const newLikes = (multiplicationValue * this.props.data.betAssesment.averageLikes).toFixed(0)
         const newDislikes = (multiplicationValue * this.props.data.betAssesment.averageDislikes).toFixed(0)
+        console.log(this.state.betViews, this.props.data.videoStatistics)
+        // if (this.state.betViews >= parseInt(this.props.data.videoStatistics.viewCount)) {
+        //     this.state.betAmount = (this.state.value - 30) * this.state.value
+        //     console.log(this.state.betAmount)
+        // }
 
 
         this.setState({
@@ -30,6 +38,11 @@ export default class MakeBet extends React.Component {
             betLikes: newLikes,
             betDislikes: newDislikes
         })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        window.alert(this.state.value)
     }
 
     render() {
@@ -67,9 +80,12 @@ export default class MakeBet extends React.Component {
                             <p><FaRegThumbsUp />{this.numberWithCommas(this.state.betLikes)}</p>
                             <p><FaRegThumbsDown />{this.numberWithCommas(this.state.betDislikes)}</p>
                         </div>
+                        <form onSubmit={this.handleSubmit}>
                         <div className="dashboard-bet-slider-container">
                             <input type="range" min="0" max="100" value={this.state.value} onChange={this.updateValue} step="1" className="dashboard-bet-slider-button"/>
                         </div>
+                        <button type="submit" className="dasboard-bet-submit-button"><h3>BET</h3></button>
+                        </form>
                     </div>
                     </div>
             </React.Fragment>
